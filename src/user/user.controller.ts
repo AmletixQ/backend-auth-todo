cf0f1fc4c8713a3766e46359454b88da19cefc2a
AmletixQ <nazarovamridin308@gmail.com>
2023-08-29 16:38:43 +0300

    Add signin function for user-module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserResponseInterface } from './types/userResponse.interface';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  async signin(@Body("user") createUserDTO: CreateUserDto): Promise<UserResponseInterface> {
+    const user = await this.userService.signin(createUserDTO);
+    return this.userService.buildUserResponse(user);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserEntity } from "./user.entity";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { sign } from "jsonwebtoken";
+
+import * as dotenv from "dotenv";
+import { UserResponseInterface } from "./types/userResponse.interface";
+dotenv.config();
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async signin(createUserDto: CreateUserDto) {
+    const userByEmail = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    const userByUsername = await this.userRepository.findOne({
+      where: {
+        username: createUserDto.username,
+      },
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        "Email or username are taken",
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const user = new UserEntity();
+    Object.assign(user, createUserDto);
+
+    return await this.userRepository.save(user);
+  }
+
+  private generate(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET_KEY,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    delete user.password;
+    return {
+      user: {
+        ...user,
+        token: this.generate(user),
+      },
+    };
+  }
+}
