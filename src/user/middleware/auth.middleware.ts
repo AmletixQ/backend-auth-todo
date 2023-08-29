e255e55f9df805986c633c9b735afb059b15b9b7
AmletixQ <nazarovamridin308@gmail.com>
2023-08-29 17:16:59 +0300

    Add middleware for check authentification

---
+++ src/user/middleware/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { NestMiddleware } from "@nestjs/common";
+import { UserService } from "../user.service";
+import { ExpressRequestInterface } from "src/types/ExpressRequest.interface";
+import { NextFunction, Response } from "express";
+import { JwtPayload, verify } from "jsonwebtoken";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly userService: UserService) {}
+
+  async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
+    if (!req.headers.authorization) {
+      req.user = null;
+      next();
+      return;
+    }
+
+    const token = req.headers.authorization.split(" ")[1];
+    try {
+      const decode = verify(token, process.env.JWT_SECRET_KEY) as JwtPayload;
+      const user = await this.userService.findByID(decode.id);
+      req.user = user;
+    } catch (error) {
+      req.user = null;
+      next();
+    }
+  }
+}
