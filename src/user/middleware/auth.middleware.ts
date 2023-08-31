68809e93a1213fcd61caef4ff6e44ff2d45aa650
AmletixQ <nazarovamridin308@gmail.com>
2023-08-31 11:56:46 +0300

    Resetting for cookie logic

--- src/user/middleware/auth.middleware.ts
+++ src/user/middleware/auth.middleware.ts
@@ -11,13 +11,14 @@ export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly userService: UserService) {}
 
   async use(req: ExpressRequestInterface, res: Response, next: NextFunction) {
-    if (!req.headers.authorization) {
+    const token: string = req.cookies["auth-token"];
+    if (!token) {
       req.user = null;
       next();
       return;
     }
 
-    const token = req.headers.authorization.split(" ")[1];
+    // const token = req.headers.authorization.split(" ")[1];
     try {
       const decode = verify(token, process.env.JWT_SECRET_KEY) as JwtPayload;
       const user = await this.userService.findByID(decode.id);
